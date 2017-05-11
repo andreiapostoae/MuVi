ae15752815ee4e74d9e3428b9e6fd3cd0df3d8e1
andreiaciobanitei <aciobanitei34@gmail.com>
2017-05-11 22:40:34 +0300

    conectat la baza de date--cand faci signup pe user nou, il adauga in baza de date

--- app/src/main/java/com/samaras/muvi/LoginActivity.java
+++ app/src/main/java/com/samaras/muvi/LoginActivity.java
@@ -1,43 +1,75 @@
 package com.samaras.muvi;
 
-import android.os.AsyncTask;
+import android.icu.lang.UProperty;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.samaras.muvi.Backend.ClientHTTP;
-import com.samaras.muvi.Backend.JSONAsyncTask;
-import com.samaras.muvi.Backend.MovieInfo;
-import com.samaras.muvi.Backend.MovieList;
-
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    DatabaseReference rf = FirebaseDatabase.getInstance().getReference("users");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
 
-        String URL = ClientHTTP.createURL("/movie/now_playing");
-        JSONAsyncTask task = new JSONAsyncTask(URL);
-        task.
-        MovieList.movies = new ArrayList<MovieInfo>();
-        task.execute();
-        try {
-            task.get(2000, TimeUnit.MILLISECONDS);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        final EditText email = (EditText) findViewById(R.id.editTextEmail);
+        final EditText parola = (EditText) findViewById(R.id.editTextParola);
 
-        System.out.println("size: " + MovieList.movies.size());
+        Button buttonCr = (Button) findViewById(R.id.buttonCreateUser);
+        Button buttonLg = (Button) findViewById(R.id.buttonLogIn);
+
+
+        buttonCr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String stringEmail = email.getText().toString();
+                String stringParola = parola.getText().toString();
+                mAuth.createUserWithEmailAndPassword(stringEmail, stringParola).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            //Log.i("ceva intr", "vceva" );
+                            rf.child(mAuth.getCurrentUser().getUid().toString()).setValue(new UserObj(stringEmail));
+                        }
+                    }
+                });
+            }
+        });
+
+        buttonLg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String stringEmail = email.getText().toString();
+                String stringParola = parola.getText().toString();
+                mAuth.signInWithEmailAndPassword(stringEmail, stringParola).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        Toast.makeText(getApplicationContext(), "te-ai logat",
+//                                Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
 
     }
-
-
-
 }
